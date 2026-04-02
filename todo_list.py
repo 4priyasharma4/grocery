dd550cc9faee40424c8b659230b0fe2bdbe8f6ef
4priyasharma4 <4priyasharma4@gmail.com>
2026-04-02 21:45:59 +0200

    Add print statement to items.  and update Readme file

---
+++ todo_list.py
@@ -0,0 +1,13 @@
+class Todo_List:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def remove_item(self, item):
+        self.items.remove(item)
+
+    def display_items(self):
+        for item in self.items:
+            print(item)
